0243028c17e52e1c053c3fbf7928c72b3231f847
App Academy Student <student@appacademy.io>
2016-09-16 14:02:43 -0700

    implement #new_move_positions & ::valid_moves

--- knight_travails.rb
+++ knight_travails.rb
@@ -8,9 +8,26 @@ class KnightPathFinder
   end
 
   def self.valid_moves(pos)
+    x, y = pos
+    return false unless (0..7).include?(x) && (0..7).include?(y)
+    true
   end
 
-  def new_move_positions
+  def new_move_positions(pos)
+    possible_positions = []
+    x, y = pos
+
+    [-2, 2].each do |long|
+      [-1, 1].each do |short|
+        move = [x + long, y + short]
+        possible_positions << move if KnightPathFinder.valid_moves(move)
+
+        move = [x + short, y + long]
+        possible_positions << move if KnightPathFinder.valid_moves(move)
+      end
+    end
+
+    possible_positions
   end
 
   def build_move_tree
